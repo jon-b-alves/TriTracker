333a929d3a3a458e955b50c95bd064f3cd21ea71
jon-b-alves <alves.b.jon@gmail.com>
2025-05-01 15:45:56 -0400

    add ability to redirect to main page after loging in. also added session.

--- tri_tracker/routes.py
+++ tri_tracker/routes.py
@@ -1,30 +1,23 @@
-from flask import render_template, request, jsonify, redirect, url_for, flash
+from flask import render_template, request, jsonify, redirect, url_for, flash, session
 from tri_tracker.models import User, Workout, db
 
 def init_routes(app):
     @app.route("/")
     def index():
         users = User.query.all()
-        return render_template("index.html", users=users)
-
-    @app.route("/user/<int:user_id>", methods=["GET"])
-    def get_user(user_id):
-        user = User.query.get(user_id)
-        return jsonify({"username": user.username})
+        username = session.get("username")
+        if username:
+            return render_template("index.html", username=username)    
+        return render_template("login.html", users=users)
     
-    @app.route("/test", methods=["POST"])
-    def test():
-        data = request.get_json()
-        new_user = User(username=data["username"])
-        db.session.add(new_user)
-        db.session.commit()
-        return jsonify({"message": "User created successfully!"}), 201
     
-    @app.route("/login", methods=["POST"])
+    @app.route("/login", methods=["POST", "GET"])
     def login():
-        x=1
-    
+        username = request.form.get("username")
+        session["username"] = username
+        return redirect(url_for("index"))
 
+    
     @app.route("/create-user", methods=["POST"])
     def create_user():
         username = request.form.get("new_username").lower()
@@ -32,9 +25,9 @@ def init_routes(app):
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
             flash("username already exists")
-            return redirect(url_for("index"))
+            return redirect(url_for("login"))
             
         db.session.add(User(username=username))
         db.session.commit()
         flash("user created successfully")
-        return redirect(url_for("index"))
+        return redirect(url_for("login"))
